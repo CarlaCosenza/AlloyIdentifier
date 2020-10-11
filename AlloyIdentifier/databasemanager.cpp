c03f6f571693344c6d0830cfa10ec5326ece490e
Carla Cosenza <carlaszecosenza@hotmail.com>
2020-10-11 03:53:23 -0300

    Small changes in saving csv file

--- AlloyIdentifier/databasemanager.cpp
+++ AlloyIdentifier/databasemanager.cpp
@@ -29,22 +29,22 @@ bool DatabaseManager::writeAlloyToDatabase(Alloy alloy){
 
 QString DatabaseManager::parseAlloyForDatabase(Alloy alloy){
     QString parsedAlloy = alloy.getName();
-    parsedAlloy.append(", ");
+    parsedAlloy.append(",");
     AlloyClass alloyClass = alloy.getAlloyClass();
     parsedAlloy.append(enumOp.alloyClassToString(alloyClass));
-    parsedAlloy.append(", ");
+    parsedAlloy.append(",");
     for(int i = MAGNESIUM ; i <= CARBON ; i++) {
         Elements el = static_cast<Elements>(i);
         Composition composition = alloy.getCompositionOfElement(el);
         parsedAlloy.append(QString::number(composition.getMin()));
-        parsedAlloy.append(", ");
+        parsedAlloy.append(",");
         parsedAlloy.append(QString::number(composition.getMax()));
-        parsedAlloy.append(", ");
+        parsedAlloy.append(",");
         parsedAlloy.append(QString::number(composition.getSpec()));
-        parsedAlloy.append(", ");
+        parsedAlloy.append(",");
     }
 
-    parsedAlloy.chop(2);
+    parsedAlloy.chop(1);
     parsedAlloy.append("\n");
 
     return parsedAlloy;
